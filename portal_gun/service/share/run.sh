540448324c5ae7475e8e9ff665b6ab96d30ad11a
ss8651twtw <ss8651twtw@gmail.com>
2020-05-25 04:54:22 +0800

    Fix the environment error in run.sh

--- portal_gun/service/share/run.sh
+++ portal_gun/service/share/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 exec 2> /dev/null
-timeout 60 LD_PRELOAD=/home/portal_gun/hook.so /home/portal_gun/portal_gun
+LD_PRELOAD=/home/portal_gun/hook.so timeout 60 /home/portal_gun/portal_gun
